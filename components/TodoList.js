161d6f7f722a093da82b32565b6520d441b68ecd
Sonila Mohanty <48008901+Sonilakitu@users.noreply.github.com>
2023-05-13 23:53:31 +0530

    Update TodoList.js

--- components/TodoList.js
+++ components/TodoList.js
@@ -5,6 +5,7 @@ function TodoList() {
   const [todos, setTodos] = useState([]);
   const addTodo = (todo) => {
     if (!todo.text || /^\s*$/.test(todo.text)) {
+      
       return;
     }
     const newTodos = [todo, ...todos];
